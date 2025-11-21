0bf09bd8347cde47c244ce639a71cf9b6a0244ce
Vinicius Rambo <vncs.rambo@gmail.com>
2025-11-21 19:19:06 -0300

    Add files via upload

---
+++ 15-API/app/Controller/ClubeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use App\Dao\ClubeDAO;
+use App\Mapper\ClubeMapper;
+use App\Service\ClubeService;
+use App\Util\MensagemErro;
+
+use \PDOException;
+
+class ClubeController {
+
+	private ClubeDAO $clubeDAO;
+	private ClubeMapper $clubeMapper;
+	private ClubeService $clubeService;
+
+	public function __construct() {
+		$this->clubeDAO = new ClubeDAO();
+		$this->clubeMapper = new ClubeMapper();
+		$this->clubeService = new ClubeService();
+	}
+
+	public function listar(Request $request, Response $response, array $args): Response {
+		$clubes = $this->clubeDAO->list();
+		//$response->getBody()->write(print_r($clubes, true));
+
+		$json = json_encode($clubes, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		$response->getBody()->write($json);
+
+		return $response
+			->withHeader('Content-Type', 'application/json')
+			->withStatus(200);
+    }
+
+	public function buscarPorID(Request $request, Response $response, array $args): Response {
+		$id = $args["id"];
+		$clubes = $this->clubeDAO->findById($id);
+		//$response->getBody()->write(print_r($clubes, true));
+
+		$json = json_encode($clubes, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		$response->getBody()->write($json);
+
+		return $response
+			->withHeader('Content-Type', 'application/json')
+			->withStatus(200);
+    }
+}
---
+++ 15-API/app/Util/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Util;
+
+class Config {
+
+    //Configurar essas variáveis de acordo com o seu ambiente
+    const DB_HOST = "localhost";
+    const DB_NAME = "futebol";
+    const DB_USER = "root";
+    const DB_PASSWORD = "bancodedados";
+}
